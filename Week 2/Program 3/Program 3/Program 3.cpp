5af834151dd9574e29aa7f79a106d8612657f1a1
lydgreeny <107149636+lydgreeny@users.noreply.github.com>
2022-11-23 12:36:58 +0000

    Program 3

---
+++ Week 2/Program 3/Program 3/Program 3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int myInt1 = 5, myInt2;
+	myInt2 = 10;
+
+	cout << "Value stored in myInt1 is " << myInt1 << endl;
+	cout << "Value stored in myInt2 is " << myInt2 << endl;
+
+	float myFloat1 = 1.2f, myFloat2;
+	myFloat2 = 9.9f;
+
+	cout << "Value stored in myFloat1 is " << myFloat1 << endl;
+	cout << "Value stored in myFloat2 is " << myFloat2 << endl;
+
+	char myChar = 'a';
+
+	cout << "Value stored in myChar is " << myChar << endl;
+
+	return 0;
+}
