0a31f9b10235ab95b1c2d12781981e4caa4e62ba
lydgreeny <107149636+lydgreeny@users.noreply.github.com>
2022-11-23 13:08:11 +0000

    Program 6

---
+++ Week 2/Program 6/Program 6/Program 6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+int width = 15;
+int height = 25;
+int area = width * height;
+
+cout << "The area of a " << width << " by " << height << " rectangle is " << area << endl; 
+}
