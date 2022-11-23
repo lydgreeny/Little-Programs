33191474aeb16b5b3f3c6985b65e885e8fc1b48e
lydgreeny <107149636+lydgreeny@users.noreply.github.com>
2022-11-23 14:20:29 +0000

    Program 8 rando

---
+++ Week 2/Program 8/Program 8/Program 8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+const float pi = 3.14159f;
+float radius = 25.0f;
+float area = pi * radius * radius;
+
+int main()
+{
+	cout << "The area of a circle with a radius of " << radius << " is " << area << endl;
+}
