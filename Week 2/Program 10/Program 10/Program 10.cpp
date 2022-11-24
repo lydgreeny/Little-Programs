8105cab85be56c9b3cd0053edcab5eff91a3fe87
lydgreeny <107149636+lydgreeny@users.noreply.github.com>
2022-11-24 17:15:00 +0000

    program 10 still not finished

--- Week 2/Program 10/Program 10/Program 10.cpp
+++ Week 2/Program 10/Program 10/Program 10.cpp
@@ -4,14 +4,10 @@ using namespace std;
 int num_of_haribos = 40;
 int num_of_students = 14;
 int num_of_bags_each = num_of_haribos / num_of_students;
-int bags_left_over = num_of_haribos / num_of_bags_each;
 int food_left_for_me = num_of_haribos - (num_of_haribos / num_of_bags_each);
 
 int main()
 {
-	num_of_bags_each
-
-	cout << "Each student gets " << num_of_bags_each << " packets" >> endl;
+	cout << "Each student gets " << num_of_bags_each << " packets" << endl;
 	cout << "Left over for me " << food_left_for_me << " packets" << endl;
-
 }
