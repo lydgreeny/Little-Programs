ebddc2bbeceae61a09c54918d68e04df2a25271a
Lydia <107149636+lydgreeny@users.noreply.github.com>
2022-11-25 15:19:35 +0000

    program 12 getting there

--- Week 2/Program 12/Program 12/Program 12.cpp
+++ Week 2/Program 12/Program 12/Program 12.cpp
@@ -10,30 +10,39 @@ char temp;
 
 int main()
 {
-    cout << "Tell me if its raining with 1 or snowing with 2" << weather << endl;
-    cin >> weather;
+cout << "Tell me if its raining with 1 or snowing with 2" << weather << endl;
+cin >> weather;
 
-    cout << "What is the temperature in celsius" << temp << endl;
-    cin >> temp;
+cout << "What is the temperature in celsius" << temp << endl;
+cin >> temp;
 
-    if (weather == 1)
+if (weather == 1)
+{
+    if (temp > 15)
     {
-    cout 
-    }
-    else()
-    {
-    }
-
-    if ()
-    {
-    }
-    else if ()
-    {
+        cout << "Wear a light raincoat" << endl;
     }
     else
     {
+        cout << "Wear a thick coat" << endl;
     }
-
+}
+if (weather == 2)
+{
+    if (temp >5)
+    {
+        cout << "Wrap up arm" << endl;
+    }
+    else if (temp <= 0);
+    {
+        cout << "Wrap up super warm" << endl;
+    }
+    else;
+    {
+        cout << "Have a nice day! It is neither raining or snowing" << endl;
+    }
+}
+    return 0;
 
 }
 
