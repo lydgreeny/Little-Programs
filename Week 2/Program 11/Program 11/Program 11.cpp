8d73b6b140b74e3a934bea91ac7e3e3e6b83ef09
lydgreeny <107149636+lydgreeny@users.noreply.github.com>
2022-11-25 12:05:27 +0000

    program 11 not finsihed

--- Week 2/Program 11/Program 11/Program 11.cpp
+++ Week 2/Program 11/Program 11/Program 11.cpp
@@ -1,20 +1,40 @@
-// Program 11.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std; 
+
+int playerInput;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+cout << "Choose your difficulty! (at your own risk, player's discretion is advised)" << endl;
+cout << "A little scary but not too much" << endl;
+cout << "Your avarage joe" << endl;
+cout << "Challenge me!" << endl;
+cout << "Sicko mode" << endl;
+cin >> playerInput;
+
+switch (playerInput)
+	{
+	case "A little scary but not too much":
+		cout << "get cozy!" << endl;
+	break;
+
+	case"Your avarage joe":
+		cout << "so avarage" << endl;
+	break;
+
+
+	case "Challenge me!":
+		cout << "ard as nails" << endl;
+	break;
+
+	case "Sicko mode":
+		cout << "phf good luck!" << endl;
+	break;
+
+	default:
+		cout << "That isn't a game mode, how funny, try again" << endl;
+	break;
+	}
+
+return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
