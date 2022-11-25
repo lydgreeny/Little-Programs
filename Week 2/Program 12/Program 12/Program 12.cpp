42017731c032c1380ec219e02662097c8fbf14f2
lydgreeny <107149636+lydgreeny@users.noreply.github.com>
2022-11-25 22:53:11 +0000

    program 12 finished

--- Week 2/Program 12/Program 12/Program 12.cpp
+++ Week 2/Program 12/Program 12/Program 12.cpp
@@ -3,44 +3,45 @@ using namespace std;
 
 int rain = 1;
 int snow = 2;
-
-char weather;
-char temp;
-
+int weather;
+int temp;
 
 int main()
 {
-cout << "Tell me if its raining with 1 or snowing with 2" << weather << endl;
-cin >> weather;
 
-cout << "What is the temperature in celsius" << temp << endl;
-cin >> temp;
 
-if (weather == 1)
-{
-    if (temp >= 15)
-    {
-        cout << "Wear a light raincoat" << endl;
-    }
-    else(temp <= 15);
-    {
-        cout << "Wear a thick coat" << endl;
-    }
-}
-if (weather == 2)
-{
-   if (temp > 5)
-   {
-        cout << "Wrap up arm" << endl;
-   }
-   else if (temp <= 0)
-   {
-        cout << "Wrap up super warm" << endl;
-   }
-   else
-   {
-        cout << "Have a nice day! It is neither raining or snowing" << endl;
-   }
-}
-}
+	cout << "Tell me if its raining with 1 or snowing with 2"  << endl;
+	cin >> weather;
 
+	cout << "What is the temperature in celsius" << endl;
+	cin >> temp;
+
+
+	if (weather == 1)
+	{
+	    if (temp > 15)
+		{
+			cout << "Wear a light raincoat" << endl;
+		}
+		else;
+		{
+			cout << "Wear a thick coat" << endl;
+		}
+	}
+	if (weather == 2)
+	{
+		if (temp > 5)
+		{
+			cout << "Wrap up warm" << endl;
+		}
+		else if (temp > 0)
+		{
+			cout << "Wrap up super warm" << endl;
+		}
+		else
+		{
+			cout << "Have a nice day! It is neither raining or snowing" << endl;
+		}
+		//}
+	}
+}
