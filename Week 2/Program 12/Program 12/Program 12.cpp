c12c8df02e306e67adcbc47c37bf2a9edda3c5fb
lydgreeny <107149636+lydgreeny@users.noreply.github.com>
2022-11-25 12:28:21 +0000

    program 12 started

---
+++ Week 2/Program 12/Program 12/Program 12.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int rain = 1;
+int snow = 2;
+
+char weather;
+char temp;
+
+
+int main()
+{
+    cout << "Tell me if its raining with 1 or snowing with 2" << weather << endl;
+    cin >> weather;
+
+    cout << "What is the temperature in celsius" << temp << endl;
+    cin >> temp;
+
+    if (weather == 1)
+    {
+    cout 
+    }
+    else()
+    {
+    }
+
+    if ()
+    {
+    }
+    else if ()
+    {
+    }
+    else
+    {
+    }
+
+
+}
+
