96cc68bc7f1cc3612ac213ddb4c2dce0c06e73a4
Lydia <107149636+lydgreeny@users.noreply.github.com>
2022-12-01 09:38:41 +0000

    program 14 finished

--- Week 3/Program 14/Program 14/Program 14.cpp
+++ Week 3/Program 14/Program 14/Program 14.cpp
@@ -1,20 +1,28 @@
-// Program 14.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+int num1;
+int num2;
+int two = 2;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	while (true)
+	{
+		cout << "Please enter a number " << endl;
+		cin >> num1;
+		cout << "please enter another number " << endl;
+		cin >> num2;
+
+		//debug 
+	    //cout << num1 << endl;
+	    //cout << num2 << endl;
+
+		int answer = (num1 + num2) / two;
+
+		cout << "The mean average of your numbers is " << answer << endl;
+
+		if (num1 == 0 || num2 == 0)
+			break;
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
