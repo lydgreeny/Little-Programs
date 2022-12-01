66d4e7d3a9e05e90d13fe75e1ca5cf4aed2c6a5c
Lydia <107149636+lydgreeny@users.noreply.github.com>
2022-12-01 09:56:40 +0000

    program 15 created

---
+++ Week 3/Program 15/Program 15/Program 15.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int playerInput;
+
+int main()
+{
+	cout << "Give me a number between 1-10" << endl;
+	cin >> playerInput;
+	for (playerInput)
+	{
+	}
+
+	cout << "Would you like to go again?" << endl;
+	cin >> playerInput;
+
+	if (playerInput == 'Y')
+	{
+		cout << "" << endl;
+	}
+	else
+	{
+		cout << "" << endl;
+	}
+}
+
