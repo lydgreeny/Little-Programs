495c4233aab06ccc46feeb52390b9a7b98642e9f
Lydia <107149636+lydgreeny@users.noreply.github.com>
2022-12-02 12:37:37 +0000

    program 17 prep

---
+++ Week 4/Program 17/Program 17/Program 17.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std; 
+
+
+
+void addTwoNumbers(int num1, int num2)
+{
+    cout << num1 << "+" << num2 << "=" << num1 + num2 << endl;
+}
+
+int main()
+{
+    addTwoNumbers(10, 5 )
+
+
+
+
+    return 0;
+}
