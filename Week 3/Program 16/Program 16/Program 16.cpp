85fa0c1bb794d2c8fe658dc220403d9e0a97a718
Lydia <107149636+lydgreeny@users.noreply.github.com>
2022-12-02 11:44:06 +0000

    program 16 done bitch

--- Week 3/Program 16/Program 16/Program 16.cpp
+++ Week 3/Program 16/Program 16/Program 16.cpp
@@ -8,8 +8,21 @@ int main()
 {
     cout << "Player life = " << playerLife << endl;
 
-    cout << "How much damage should i deal? " << playerInput << endl;
-    cin >> playerInput;
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << "How much damage would you like to deal? " << endl;
 
-    for (int playerLife; i < 10; 
+        cin >> playerInput;
+        playerLife -= playerInput;
+
+        if (playerLife <= 0)
+        {
+            cout << "Early end, Player died! :> " << endl;
+            break;
+        }
+    }
+    if (playerLife > 0)
+    {
+        cout << "All questions asked, Player survived :< " << endl;
+    }
 }
