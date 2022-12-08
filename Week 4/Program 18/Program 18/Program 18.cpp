0956221c7feafd6273bbf8f3118a8ff47f0ec08b
Lydia <107149636+lydgreeny@users.noreply.github.com>
2022-12-08 10:53:33 +0000

    program 18 in progress

--- Week 4/Program 18/Program 18/Program 18.cpp
+++ Week 4/Program 18/Program 18/Program 18.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 using namespace std;
 
+int userInput;
+void smallestNumver(int n1, int n2);
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "Please imput a number" << endl;
+        cin >> userInput;
+		cout << "and another" << endl;
+		cin >> userInput;
+	}
+	void smallestNumber(int n1, int n2);
+	{
+
+
+	}
+
+
 }
 
