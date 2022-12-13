a55aa9c67a9c5295b96c7d9607fa37710b1f647d
lydgreeny <107149636+lydgreeny@users.noreply.github.com>
2022-12-13 21:40:20 +0000

    program 18 bug fixing

--- Week 4/Program 18/Program 18/Program 18.cpp
+++ Week 4/Program 18/Program 18/Program 18.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 int userInput1;
 int userInput2;
-void smallestNumver(int n1, int n2);
+int userInput3;
+int answer = userInput1 *= 10;
+int answer2 = answer /= 3;
+void smallestNumber(int n1, int n2);
 
 int main()
 {
@@ -26,17 +29,36 @@ int main()
 		}
 		else
 		{
-		cout << "Both are equil " << endl;
-		cout << endl;
-        }
-		
-	}
-	void smallestNumber(int n1, int n2);
-	{
-
+			cout << "Both are equil " << endl;
+			cout << endl;
+			cout << endl;
+		}
 
 	}
-
 
 }
+	void smallestNumber(int n1, int n2);
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			cout << "Please imput a number to be multipled by 10 " << endl;
+			cin >> userInput1;
+			cout << userInput1 << "divided by 3 = " << answer2 << endl;
+			cout << endl;
+
+
+			cout << "and another" << endl;
+			cin >> userInput2;
+			cout << userInput2 << "divided by 3 = " << answer2 << endl;
+			cout << endl;
+
+			cout << "and another" << endl;
+			cin >> userInput3;
+			cout << userInput2 << "divided by 3 = " << answer2 << endl;
+			cout << endl;
+
+		}
+	}
+
+
 
