7424d28d8981519c73705b50019c9baf51dbe10b
lydgreeny <107149636+lydgreeny@users.noreply.github.com>
2022-12-13 16:52:03 +0000

    program 18 if statemet sorted

--- Week 4/Program 18/Program 18/Program 18.cpp
+++ Week 4/Program 18/Program 18/Program 18.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int userInput;
+int userInput1;
+int userInput2;
 void smallestNumver(int n1, int n2);
 
 int main()
@@ -9,9 +10,26 @@ int main()
 	for (int i = 0; i < 3; i++)
 	{
 		cout << "Please imput a number" << endl;
-        cin >> userInput;
+		cin >> userInput1;
 		cout << "and another" << endl;
-		cin >> userInput;
+		cin >> userInput2;
+
+		if (userInput1 > userInput2)
+		{
+			cout << userInput2 << " is the smaller number" << endl;
+			cout << endl;
+		}
+		else if (userInput1 < userInput2)
+		{
+			cout << userInput1 << " is the smaller number" << endl;
+			cout << endl;
+		}
+		else
+		{
+		cout << "Both are equil " << endl;
+		cout << endl;
+        }
+		
 	}
 	void smallestNumber(int n1, int n2);
 	{
