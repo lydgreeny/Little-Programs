b953e99c50b79948f006c09d4fa5258a0d3ed3d3
Lydia <107149636+lydgreeny@users.noreply.github.com>
2022-12-15 11:53:01 +0000

    program 19 updtaed

--- Week 4/Program 19/Program 19/Program 19.cpp
+++ Week 4/Program 19/Program 19/Program 19.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
 using namespace std;
+int numOfOdd;
+int oddTotal;
+int numOfEven;
+int evenTotal;
+bool oddOReven(int n1);
+void outputResults(int numOfOdd, int oddTotal, int numOfEven, int evenTotal);
 
 int main()
 {
-    cout << "Hello World!\n";
+	int userInput;
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "Please input a number" << endl;
+		cin >> userInput;
+		oddOReven(userInput);
+	}
+	outputResults(numOfOdd, oddTotal, numOfEven, evenTotal);
+//debug
+//cout << numOfEven << endl;
+//cout << numOfOdd << endl;
+}
+
+bool oddOReven(int n1)
+{
+	if (n1 % 2 == 0)
+	{
+	    numOfEven++;
+	   // evenTotal + n1;
+	}
+	else
+	{
+		numOfOdd++;
+		//oddTotal;
+	}
+	return n1;
+}
+
+void outputResults(int numOfOdd, int oddTotal, int numOfEven, int evenTotal)
+{
+	cout << "Number of odd = " << numOfOdd<< endl;
+    cout << "Number of even = " << numOfEven << endl;
 }
